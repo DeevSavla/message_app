711f523b81bb0de01114687aa7deb8658298381b
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-07-26 10:53:36 +0530

    debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,29 @@
-import { NextResponse, NextRequest } from 'next/server'
-export { default } from "next-auth/middleware"
-import { getToken } from "next-auth/jwt"
+import { NextResponse, NextRequest } from 'next/server';
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
 
-// See "Matching Paths" below to learn more
 export const config = {
     matcher: ['/sign-in', '/signup', '/', '/dashboard/:path*', '/verify/:path*'],
-}
+};
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
 
-    const token = await getToken({ req: request })
-    const url = request.nextUrl
-
-    if (token &&
-        (
+    if (token) {
+        if (
             url.pathname.startsWith('/sign-in') ||
             url.pathname.startsWith('/signup') ||
-            url.pathname.startsWith('/verify') ||
-            url.pathname.startsWith('/')
-        )
-    ) {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
-
-    if (!token && url.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
+            url.pathname.startsWith('/verify')
+        ) {
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        }
+    } else {
+        if (url.pathname.startsWith('/dashboard')) {
+            if (url.pathname !== '/sign-in') {
+                return NextResponse.redirect(new URL('/sign-in', request.url));
+            }
+        }
     }
 
     return NextResponse.next();
